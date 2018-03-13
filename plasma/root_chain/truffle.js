1381ea3084ffa110b51a91d5b700f7788482e19f
Jeremiah Andrews <jlandrews@berkeley.edu>
2018-03-12 21:30:52 -0700

    Removed network in truffle.js to allow automatic testing

--- plasma/root_chain/truffle.js
+++ plasma/root_chain/truffle.js
@@ -1,12 +1,12 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-    networks: {
-    development: {
-    host: "localhost",
-    port: 7545,
-    network_id: "*" // Match any network id
-   }
-  }
+//    networks: {
+//   development: {
+//    host: "localhost",
+//    port: 7545,
+//    network_id: "*" // Match any network id
+//   }
+//  }
  
 };
