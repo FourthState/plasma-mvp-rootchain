bbc0aa7ee65653e1e1df77b22170e23bd51668d7
Hamdi Allam <hamdi.allam97@gmail.com>
2018-03-14 02:17:45 -0700

    listen to ganache

--- plasma/root_chain/truffle.js
+++ plasma/root_chain/truffle.js
@@ -1,12 +1,11 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-//    networks: {
-//   development: {
-//    host: "localhost",
-//    port: 7545,
-//    network_id: "*" // Match any network id
-//   }
-//  }
- 
+    networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  }
 };
