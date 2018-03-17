25adad7486e1470b3a2beb5fd67f80d4a93a7af5
legengliu <liulegeng@gmail.com>
2018-03-17 01:12:06 -0700

    test that currentChildBlock increments correctly after deposit and block submission

--- test/rootchain.js
+++ test/rootchain.js
@@ -28,13 +28,33 @@ contract('RootChain', async (accounts) => {
         let txBytes = RLP.encode([0, 0, 0, 0, 0, 0, accounts[2], depositAmount, 0, 0, 0]);
         let prev =  parseInt(await instance.currentChildBlock.call());
 
-        let result = await instance.deposit(txBytes.toString('binary'), {'from': accounts[2], 'value': depositAmount}).then(res => {return res});
+        let result = await instance.deposit(txBytes.toString('binary'), {'from': accounts[2], 'value': depositAmount});
         assert.equal(result.logs[0].args.depositor, accounts[2], 'Deposit event does not match depositor address.');
         assert.equal(parseInt(result.logs[0].args.amount), depositAmount, 'Deposit event does not match deposit amount.');
 
         let curr = parseInt(await instance.currentChildBlock.call());
         assert.equal(prev + 1, curr, "Child block did not increment");
         // TODO: check correctness of newly created merkle root
+    });
+
+    it("Deposit then submit block passes", async () => {
+        let instance = await RootChain.deployed();
+        let depositAmount = 50000;
+        let txBytes = RLP.encode([0, 0, 0, 0, 0, 0, accounts[2], depositAmount, 0, 0, 0]);
+        let prevBlockNum = parseInt(await instance.currentChildBlock.call());
+
+        await instance.deposit(txBytes.toString('binary'), {'from': accounts[2], 'value': depositAmount});
+        let currBlockNum = parseInt(await instance.currentChildBlock.call());
+        assert.equal(prevBlockNum + 1, currBlockNum, "Child block did not increment after Deposit.");
+
+        for (i = 0; i < 5; i++) {
+            await web3.eth.sendTransaction({'from': accounts[0], 'to': accounts[1], 'value': 100});
+        }
+
+        let blockRoot = '2984748479872';
+        await instance.submitBlock(web3.fromAscii(blockRoot));
+        let nextBlockNum = parseInt(await instance.currentChildBlock.call());
+        assert.equal(currBlockNum + 1, nextBlockNum, "Child block did not increment after submitting a block.")
     });
 
     it("Invalid deposits fail", async () => {
