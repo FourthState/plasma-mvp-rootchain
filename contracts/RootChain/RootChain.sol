2ce86be44e12cbd8f9d1440ec1f7451da4fea5ac
Hamdi Allam <hamdi.allam97@gmail.com>
2018-04-02 08:38:49 -0700

    1-index array

--- contracts/RootChain/RootChain.sol
+++ contracts/RootChain/RootChain.sol
@@ -64,8 +64,8 @@ contract RootChain {
         public
     {
         authority = msg.sender;
-        currentChildBlock = 0;
-        validatorBlocks = 0;
+        currentChildBlock = 1;
+        validatorBlocks = 1;
         lastParentBlock = block.number;
 
         exitsQueue = new PriorityQueue();
