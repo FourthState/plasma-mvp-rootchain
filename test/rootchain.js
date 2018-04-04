a2d2819cb4040b90321461eaf554482ebc2418f2
Hamdi Allam <hamdi.allam97@gmail.com>
2018-04-03 18:28:18 -0700

    await asynch calls

--- test/rootchain.js
+++ test/rootchain.js
@@ -166,13 +166,13 @@ contract('RootChain', async (accounts) => {
 
         // construct the confirm sig
         // Remove all 0x prefixes from hex strings
-        let blockHeader = (await rootchain.getChildChain(blockNum))[0];
+        let blockHeader = (await rootchain.getChildChain.call(blockNum))[0];
         let txHash = web3.sha3(txBytes.toString('hex'), {encoding: 'hex'});
         let sigs = (new Buffer(130)).toString('hex');
 
         // create the confirm sig
         let confirmHash = web3.sha3(txHash.slice(2) + sigs + blockHeader.slice(2), {encoding: 'hex'});
-        let confirmSignature = web3.eth.sign(accounts[2], confirmHash);
+        let confirmSignature = await web3.eth.sign(accounts[2], confirmHash);
 
         // start the exit
         let txPos = [blockNum, 0, 0];
@@ -189,12 +189,12 @@ contract('RootChain', async (accounts) => {
     });
 
     it("Challenge the above exit with the incorrect confirm sig", async () => {
-        let txBytes = RLP.encode([6, 0, 0, 0, 0, 0, accounts[3], 5000, 0, 0, 0]);
+        let txBytes = RLP.encode([7, 0, 0, 0, 0, 0, accounts[3], 5000, 0, 0, 0]);
         let txHash = web3.sha3(txBytes.toString('hex'), {encoding: 'hex'});
-        let sigs = web3.eth.sign(accounts[2], txHash);
+        let sigs = await web3.eth.sign(accounts[2], txHash);
         sigs += new Buffer(65).toString('hex');
 
-        let confirmSignature = web3.eth.sign(accounts[2], "INCORRECTCONFIRMSIGMUAHAHAHAHAHAHAHA-HamdiWasHere");
+        let confirmSignature = await web3.eth.sign(accounts[2], "0x6969696969696969696969");
 
         let err;
         [err] = await to(rootchain.challengeExit([7, 0, 0], [8, 0, 0],
@@ -210,7 +210,7 @@ contract('RootChain', async (accounts) => {
         // transact accounts[2] => accounts[3]. DOUBLE SPEND (earlier exit)
         let txBytes = RLP.encode([7, 0, 0, 0, 0, 0, accounts[3], 5000, 0, 0, 0]);
         let txHash = web3.sha3(txBytes.toString('hex'), {encoding: 'hex'});
-        let sigs = web3.eth.sign(accounts[2], txHash);
+        let sigs = await web3.eth.sign(accounts[2], txHash);
         sigs += new Buffer(65).toString('hex');
         let leaf = web3.sha3(txHash.slice(2) + sigs.slice(2), {encoding: 'hex'});
 
@@ -224,7 +224,7 @@ contract('RootChain', async (accounts) => {
 
         // create the right confirm sig
         let confirmHash = web3.sha3(txHash.slice(2) + sigs.slice(2) + computedRoot, {encoding: 'hex'});
-        let confirmSignature = web3.eth.sign(accounts[2], confirmHash);
+        let confirmSignature = await web3.eth.sign(accounts[2], confirmHash);
 
         let balance = (await rootchain.getBalance.call({'from': accounts[3]})).toNumber();
         assert(balance === 0, "Account balance on rootchain is not zero");
@@ -243,14 +243,18 @@ contract('RootChain', async (accounts) => {
     });
 
     it("Withdraw from the above successfull challenge", async () => {
-        let bal = web3.eth.getBalance(accounts[3]).toNumber();
-        let tx = (await rootchain.withdraw({'from': accounts[3]})).tx;
-        let gasUsed = web3.eth.getTransactionReceipt(tx).gasUsed;
-        let price = web3.eth.getTransaction(tx).gasPrice;
+        rootchainBal = await rootchain.getBalance.call({from: accounts[3]});
+        assert(rootchainBal != 0, "Account has no funds on the rootchain");
+
+        let bal = await web3.eth.getBalance(accounts[3]).toNumber();
+        let tx = await rootchain.withdraw({'from': accounts[3]});
+
+        let gasUsed = tx.receipt.gasUsed;
+        let price = (await web3.eth.getTransaction(tx.tx)).gasPrice.toNumber();
         let amt = price * gasUsed;
 
         let expectedBal = bal + 10000 - amt;
-        let currBal = web3.eth.getBalance(accounts[3]).toNumber();
+        let currBal = await web3.eth.getBalance(accounts[3]).toNumber();
 
         assert(currBal == expectedBal, "Account was not accreddited"); 
     });
@@ -264,13 +268,13 @@ contract('RootChain', async (accounts) => {
 
         // construct the confirm sig
         // Remove all 0x prefixes from hex strings
-        let blockHeader = (await rootchain.getChildChain(blockNum))[0];
+        let blockHeader = (await rootchain.getChildChain.call(blockNum))[0];
         let txHash = web3.sha3(txBytes.toString('hex'), {encoding: 'hex'});
         let sigs = (new Buffer(130)).toString('hex');
 
         // create the confirm sig
         let confirmHash = web3.sha3(txHash.slice(2) + sigs + blockHeader.slice(2), {encoding: 'hex'});
-        let confirmSignature = web3.eth.sign(accounts[2], confirmHash);
+        let confirmSignature = await web3.eth.sign(accounts[2], confirmHash);
 
         // start the exit
         let txPos = [blockNum, 0, 0];
@@ -280,13 +284,13 @@ contract('RootChain', async (accounts) => {
             hexToBinary(proofForDepositBlock), hexToBinary(exitSigs), {'from': accounts[2], 'value': 10000 });
 
         // fast forward the next block 1 week
-        let oldTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp
+        let oldTime = (await web3.eth.getBlock(web3.eth.blockNumber)).timestamp
         // a bit over a week
         await web3.currentProvider.send({jsonrpc: "2.0", method: "evm_increaseTime", params: [804800], id: 0})
         await web3.currentProvider.send({jsonrpc: "2.0", method: "evm_mine", params: [], id: 0})
 
 
-        let currTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp
+        let currTime = (await web3.eth.getBlock(web3.eth.blockNumber)).timestamp
 
         // allow for some error up 3 secs from increasing time to mining the next block
         let diff = (currTime - oldTime) - 804800
@@ -322,7 +326,7 @@ contract('RootChain', async (accounts) => {
 
         // create the confirm sig
         let confirmHash = web3.sha3(txHash.slice(2) + sigs + blockHeader.slice(2), {encoding: 'hex'});
-        let confirmSignature = web3.eth.sign(accounts[2], confirmHash);
+        let confirmSignature = await web3.eth.sign(accounts[2], confirmHash);
 
         // start the exit
         let txPos = [blockNum, 0, 0];
