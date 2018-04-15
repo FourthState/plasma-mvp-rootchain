d2a0a10648bc2e52732d06dee213ba0040d39d08
Aditya Sripal <adityasripal@gmail.com>
2018-04-14 18:15:59 -0700

    Remove migration

--- migrations/2_initial_migration.js
+++ migrations/2_initial_migration.js
@@ -1,8 +1,6 @@
 var RootChain = artifacts.require("RootChain");
-var Trap = artifacts.require("Trap");
 
 module.exports = function(deployer) {
     deployer.deploy(RootChain);
-    deployer.deploy(Trap);
   };
   
