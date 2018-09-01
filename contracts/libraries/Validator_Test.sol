274fd3b24943206e99664759552d71baefd798fd
laskdaf <kevinc_2019@berkeley.edu>
2018-08-31 19:56:26 -0700

    add validator test contract

---
+++ contracts/libraries/Validator_Test.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.4.24;
+
+import "./Validator.sol";
+
+/*
+* Used to proxy function calls to the Validator for testing
+*/
+
+contract Validator_Test {
+
+  using Validator for bytes32;
+
+  function checkMembership(bytes32 leaf, uint256 index, bytes32 rootHash, bytes proof)
+      public
+      returns (bool)
+  {
+      return leaf.checkMembership(index, rootHash, proof);
+  }
+
+  function checkSigs(bytes32 txHash, bytes32 rootHash,  uint256 blknum1, uint256 blknum2, bytes sigs)
+      public
+      returns (bool)
+  {
+      return txHash.checkSigs(bytes32 rootHash,  uint256 blknum1, uint256 blknum2, bytes sigs);
+  }
+
+  function recover(bytes32 hash, bytes sig)
+      public
+      returns (address)
+  {
+      return hash.recover(sig);
+  }
+
+  function slice(bytes _bytes, uint start, uint len)
+      public
+      returns (bytes)
+  {
+      return _bytes.slice(start, len);
+  }
+}
