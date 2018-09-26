eb66992c191c7fcf6831c95912ef6cef2c23e088
Hamdi Allam <hamdi.allam97@gmail.com>
2018-09-26 00:59:22 -0700

    finished pq

--- contracts/libraries/PriorityQueue.sol
+++ contracts/libraries/PriorityQueue.sol
@@ -4,13 +4,14 @@ pragma solidity ^0.4.24;
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 library PriorityQueue {
+    using SafeMath for uint256;
+
     function insert(uint256[] storage heapList, uint256 k)
         public
     {
         heapList.push(k);
-        uint size = heapList.length;
-        if (size > 1)
-            precUp(heapList(size - 1);
+        if (heapList.length > 1)
+            percUp(heapList, heapList.length.sub(1));
     }
 
     function getMin(uint256[] storage heapList)
@@ -30,14 +31,14 @@ library PriorityQueue {
         require(heapList.length > 0, "empty queue");
 
         uint256 min = heapList[0];
-        if (heapList.length > 1) { // the minimum can be the only element in the heap
-            // move the largest element in the heap to the top and percDown
-            heapList[0] = heapList[heapList.length - 1];
-            delete heapList[size];
-            heapList.length = heapList.length - 1;
 
-            if (heapList.length > 1)
-                percDown(heapList, 0);
+        // move the last element to the front
+        heapList[0] = heapList[heapList.length.sub(1)];
+        delete heapList[heapList.length.sub(1)];
+        heapList.length = heapList.length.sub(1);
+
+        if (heapList.length > 1) {
+            percDown(heapList, 0);
         }
 
         return min;
@@ -48,43 +49,47 @@ library PriorityQueue {
         view
         returns (uint256)
     {
-        uint size = currentSize(heapList);
-        if (i.mul(2).add(1) > size) {
-            return i.mul(2);
-        } else {
-            if (heapList[i.mul(2)] < heapList[i.mul(2).add(1)]) {
-                return i.mul(2);
-            } else {
-                return i.mul(2).add(1);
-            }
-        }
+        uint lChild = i*2 + 1;
+        uint rChild = i*2 + 2;
+
+        if (rChild > heapList.length.sub(1) || heapList[lChild] < heapList[rChild])
+            return lChild;
+        else
+            return rChild;
     }
 
     function percUp(uint256[] storage heapList, uint256 i)
         private
     {
-        uint256 j = i;
-        uint256 newVal = heapList[i];
-        while (newVal < heapList[i.div(2)]) {
-            heapList[i] = heapList[i.div(2)];
-            i = i.div(2);
+        uint256 position = i;
+        uint256 value = heapList[i];
+
+        // continue to percolate up while smaller than the parent
+        while (i != 0 && value < heapList[i.sub(1).div(2)]) {
+            heapList[i] = heapList[i.sub(1).div(2)];
+            i = i.sub(1).div(2);
         }
-        if (i != j) heapList[i] = newVal;
+
+        // place the value in the correct parent
+        if (position != i) heapList[i] = value;
     }
 
     function percDown(uint256[] storage heapList, uint256 i)
         private
     {
-        uint256 j = i;
-        uint256 newVal = heapList[i];
-        uint256 mc = minChild(heapList, i);
-        uint256 size = currentSize(heapList);
-        while (mc <= size && newVal > heapList[mc]) {
-            heapList[i] = heapList[mc];
-            i = mc;
-            mc = minChild(heapList, i);
+        uint position = i;
+        uint value = heapList[i];
+
+        // continue to percolate down while larger than the child
+        uint child = minChild(heapList, i);
+        while(child < heapList.length && value > heapList[child]) {
+            heapList[i] = heapList[child];
+            i = child;
+            child = minChild(heapList, i);
         }
-        if (i != j) heapList[i] = newVal;
+
+        // place value in the correct child
+        if (position != i) heapList[i] = value;
     }
 
     function currentSize(uint256[] storage heapList)
@@ -92,6 +97,6 @@ library PriorityQueue {
         view
         returns (uint256)
     {
-        return heapList.length.sub(1);
+        return heapList.length;
     }
 }
--- contracts/libraries/PriorityQueue_Test.sol
+++ contracts/libraries/PriorityQueue_Test.sol
@@ -6,7 +6,7 @@ import "./PriorityQueue.sol";
 contract PriorityQueue_Test {
     using PriorityQueue for uint256[];
 
-    uint256[] heapList = [];
+    uint256[] heapList;
 
     function insert(uint256 k) public { heapList.insert(k); }
     function getMin() public view returns (uint256) { return heapList.getMin(); }
