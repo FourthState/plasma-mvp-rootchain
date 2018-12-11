199ac73d82ff80916991525634ab74a5a8f3ee76
Hamdi Allam <hamdi.allam97@gmail.com>
2018-12-10 16:21:43 -0800

    removed extra indent

--- contracts/libraries/Validator.sol
+++ contracts/libraries/Validator.sol
@@ -117,44 +117,44 @@ library Validator {
             internal
             pure
             returns (bytes)
-        {
-            require(_bytes.length - start >= len, "slice out of bounds");
+    {
+        require(_bytes.length - start >= len, "slice out of bounds");
 
-            if (_bytes.length == len)
-                return _bytes;
+        if (_bytes.length == len)
+            return _bytes;
 
-            bytes memory result;
-            uint src;
-            uint dest;
+        bytes memory result;
+        uint src;
+        uint dest;
+        assembly {
+            // memory & free memory pointer
+            result := mload(0x40)
+            mstore(result, len) // store the size in the prefix
+            mstore(0x40, add(result, and(add(add(0x20, len), 0x1f), not(0x1f)))) // padding
+
+            // pointers
+            src := add(start, add(0x20, _bytes))
+            dest := add(0x20, result)
+        }
+
+        // copy as many word sizes as possible
+        for(; len >= WORD_SIZE; len -= WORD_SIZE) {
             assembly {
-                // memory & free memory pointer
-                result := mload(0x40)
-                mstore(result, len) // store the size in the prefix
-                mstore(0x40, add(result, and(add(add(0x20, len), 0x1f), not(0x1f)))) // padding
-
-                // pointers
-                src := add(start, add(0x20, _bytes))
-                dest := add(0x20, result)
+                mstore(dest, mload(src))
             }
 
-            // copy as many word sizes as possible
-            for(; len >= WORD_SIZE; len -= WORD_SIZE) {
-                assembly {
-                    mstore(dest, mload(src))
-                }
+            src += WORD_SIZE;
+            dest += WORD_SIZE;
+        }
 
-                src += WORD_SIZE;
-                dest += WORD_SIZE;
-            }
+        // copy remaining bytes
+        uint mask = 256 ** (WORD_SIZE - len) - 1;
+        assembly {
+            let srcpart := and(mload(src), not(mask)) // zero out src
+            let destpart := and(mload(dest), mask) // retrieve the bytes
+            mstore(dest, or(destpart, srcpart))
+        }
 
-            // copy remaining bytes
-            uint mask = 256 ** (WORD_SIZE - len) - 1;
-            assembly {
-                let srcpart := and(mload(src), not(mask)) // zero out src
-                let destpart := and(mload(dest), mask) // retrieve the bytes
-                mstore(dest, or(destpart, srcpart))
-            }
-
-            return result;
+        return result;
     }
 }
