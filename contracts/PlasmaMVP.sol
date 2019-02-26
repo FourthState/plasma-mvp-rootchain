5b0c8ef86da19ae2b0f4fae7a24a2e6a92113175
Hamdi Allam <hamdi.allam97@gmail.com>
2019-02-25 22:36:10 -0800

    overflow check when calculating the position

--- contracts/PlasmaMVP.sol
+++ contracts/PlasmaMVP.sol
@@ -512,7 +512,10 @@ contract PlasmaMVP {
     {
         require(validatePostion([txPos[0], txPos[1], txPos[2], 0]));
 
-        return txPos[0].mul(blockIndexFactor).add(txPos[1].mul(txIndexFactor)).add(txPos[2]);
+        uint256 position = txPos[0].mul(blockIndexFactor).add(txPos[1].mul(txIndexFactor)).add(txPos[2]);
+        require(position < 2**128-1); // check for an overflow
+
+        return position;
     }
 
     function validatePostion(uint256[4] memory position)
